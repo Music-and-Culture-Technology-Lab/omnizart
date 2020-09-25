da84e21107d2fd7c174ab6836130d3e5d137c65b
BreezeWhite <freedombluewater@gmail.com>
2020-09-25 16:42:33 +0800

    Code refactor for better modularization

--- omnizart/drum/app.py
+++ omnizart/drum/app.py
@@ -1,11 +1,8 @@
 # pylint: disable=C0103,W0612,E0611
 import os
 
-import numpy as np
-
-from omnizart.feature.cqt import extract_cqt
-from omnizart.feature.beat_for_drum import extract_mini_beat_from_audio_path
-from omnizart.drum.utils import extract_patch_cqt, create_batches, merge_batches
+from omnizart.feature.wrapper_func import extract_patch_cqt
+from omnizart.drum.prediction import predict
 from omnizart.models.spectral_norm_net import ConvSN2D
 from omnizart.utils import get_logger
 from omnizart.base import BaseTranscription
@@ -45,36 +42,17 @@ class DrumTranscription(BaseTranscription):
             raise FileNotFoundError(f"The given audio path does not exist. Path: {input_audio}")
 
         # Extract feature according to model configuration
-        logger.info("Extracting CQT feature...")
-        cqt_feature = extract_cqt(input_audio)
-
-        logger.info("Extracting beat information...")
-        mini_beat_arr = extract_mini_beat_from_audio_path(input_audio)
+        logger.info("Extracting feature...")
+        patch_cqt_feature = extract_patch_cqt(input_audio)
 
         # Load model configurations
         logger.info("Loading model...")
         model, model_settings = self._load_model(model_path, custom_objects=self.custom_objects)
 
-        logger.debug("CQT feature shape: %s", cqt_feature.shape)
-        logger.debug("Mini-beat array shape: %s", mini_beat_arr.shape)
-
         logger.info("Predicting...")
-        pred = self._predict(
-            cqt_feature, mini_beat_arr, model, model_settings.feature.mini_beat_per_segment
-        )[:len(mini_beat_arr)]  # Remove padding
-
+        pred = predict(patch_cqt_feature, model, model_settings.feature.mini_beat_per_segment)
         logger.debug("Prediction shape: %s", pred.shape)
-
         return pred
-
-    def _predict(self, cqt_feature, mini_beat_arr, model, mini_beat_per_seg, batch_size=6):  # pylint: disable=R0201
-        patch_cqt_feature = extract_patch_cqt(cqt_feature, mini_beat_arr)
-        batches = create_batches(patch_cqt_feature, mini_beat_per_seg, b_size=batch_size)
-        batch_pred = []
-        for idx, batch in enumerate(batches):
-            print(f"{idx+1}/{len(batches)}", end="\r")
-            batch_pred.append(model.predict(batch))
-        return merge_batches(np.array(batch_pred))
 
 
 if __name__ == "__main__":
