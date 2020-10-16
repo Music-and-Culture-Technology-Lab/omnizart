9f1a0763006a3dbe1eeb16544cc0a3ebd247298f
BreezeWhite <freedombluewater@gmail.com>
2020-10-16 14:16:18 +0800

    Implement a more robust parallelization approach for feature extraciton. Add training functionalities for drum transcription

--- omnizart/drum/app.py
+++ omnizart/drum/app.py
@@ -2,17 +2,25 @@
 import os
 from os.path import join as jpath
 import time
+from datetime import datetime
+import asyncio
 
 import h5py
+import tensorflow as tf
 
 from omnizart.feature.wrapper_func import extract_patch_cqt
 from omnizart.drum.prediction import predict
 from omnizart.drum.labels import extract_label_13_inst
-from omnizart.models.spectral_norm_net import ConvSN2D
+from omnizart.drum.dataset import get_dataset
+from omnizart.models.spectral_norm_net import drum_model, ConvSN2D
 from omnizart.utils import get_logger, ensure_path_exists, parallel_generator, write_yaml
+from omnizart.models.utils import shape_list
 from omnizart.base import BaseTranscription
 from omnizart.setting_loaders import DrumSettings
+from omnizart.train import train_epochs, get_train_val_feat_file_list
+from omnizart.callbacks import EarlyStopping, ModelCheckpoint
 from omnizart.constants.datasets import PopStructure
+from omnizart.constants.feature import NOTE_PRIORITY_ARRAY
 
 
 logger = get_logger("Drum Transcription")
@@ -88,7 +96,7 @@ class DrumTranscription(BaseTranscription):
             "This may take time to finish and affect the computer's performance"
         )
         assert len(train_wavs) == len(train_labels)
-        _parallel_feature_extraction(
+        _parallel_feature_extraction_v2(
             train_wavs, train_labels, train_feat_out_path, settings.feature, num_threads=num_threads
         )
 
@@ -99,7 +107,7 @@ class DrumTranscription(BaseTranscription):
             "This may take time to finish and affect the computer's performance"
         )
         assert len(test_wavs) == len(test_labels)
-        _parallel_feature_extraction(
+        _parallel_feature_extraction_v2(
             test_wavs, test_labels, test_feat_out_path, settings.feature, num_threads=num_threads
         )
 
@@ -107,6 +115,74 @@ class DrumTranscription(BaseTranscription):
         write_yaml(settings.to_json(), jpath(train_feat_out_path, ".success.yaml"))
         write_yaml(settings.to_json(), jpath(test_feat_out_path, ".success.yaml"))
         logger.info("All done")
+
+    def train(self, feature_folder, model_name=None, input_model_path=None, drum_settings=None):
+        if drum_settings is not None:
+            assert isinstance(drum_settings, DrumSettings)
+            settings = drum_settings
+        else:
+            settings = self.settings
+
+        if input_model_path is not None:
+            logger.info("Continue to train on model: %s", input_model_path)
+            model, prev_set = self._load_model(input_model_path, custom_objects=self.custom_objects)
+            settings.model.save_path = prev_set.model.save_path
+            settings.training.init_learninig_rate = prev_set.training.init_learning_rate
+            settings.training.res_block_num = prev_set.training.res_block_num
+
+        logger.info("Constructing dataset instance")
+        split = settings.training.steps / (settings.training.steps + settings.training.val_steps)
+        train_feat_files, val_feat_files = get_train_val_feat_file_list(feature_folder, split=split)
+        train_dataset = get_dataset(
+            feature_files=train_feat_files,
+            batch_size=settings.training.batch_size,
+            steps=settings.trainins.steps
+        )
+        val_dataset = get_dataset(
+            feature_files=val_feat_files,
+            batch_size=settings.training.val_batch_size,
+            steps=settings.trainins.val_steps
+        )
+
+        if input_model_path is None:
+            logger.info("Constructing new model")
+            model = drum_model(
+                out_classes=13,
+                mini_beat_per_seg=settings.feature.mini_beat_per_segment,
+                res_block_num=settings.training.res_block_num
+            )
+
+        optimizer = tf.keras.optimizers.Adam(learning_rate=settings.training.init_learning_rate)
+        model.compile(optimizer=optimizer, loss=_loss_func, metrics=["accuracy"])
+
+        logger.info("Resolving model output path")
+        if model_name is None:
+            model_name = str(datetime.now()).replace(" ", "_")
+        if not model_name.startswith(settings.model.save_prefix):
+            model_name = settings.model.save_prefix + "_" + model_name
+            model_save_path = jpath(settings.model.save_path, model_name)
+        ensure_path_exists(model_save_path)
+        write_yaml(settings.to_json(), jpath(model_save_path, "configurations.yaml"))
+        write_yaml(model.to_yaml(), jpath(model_save_path, "arch.yaml"), dump=False)
+
+        logger.info("Constructing callbacks")
+        callbacks = [
+            EarlyStopping(patience=settings.training.early_stop),
+            ModelCheckpoint(model_save_path, save_weights_only=True)
+        ]
+        logger.info("Callback list: %s", callbacks)
+
+        logger.info("Start training")
+        history = train_epochs(
+            model,
+            train_dataset,
+            validate_dataset=val_dataset,
+            epochs=settings.training.epoch,
+            steps=settings.training.steps,
+            val_steps=settings.training.val_steps,
+            callbacks=callbacks
+        )
+        return model_save_path, history
 
 
 def _parallel_feature_extraction(wav_paths, label_paths, out_path, feat_settings, num_threads=3):
@@ -151,6 +227,49 @@ def _parallel_feature_extraction(wav_paths, label_paths, out_path, feat_settings
     print("")
 
 
+def _parallel_feature_extraction_v2(wav_paths, label_paths, out_path, feat_settings, num_threads=5):
+    iter_num = len(wav_paths) / num_threads
+    if int(iter_num) < iter_num:
+        iter_num += 1
+    iter_num = int(iter_num)
+
+    label_path_mapping = _gen_wav_label_path_mapping(label_paths)
+    for iter_idx in range(iter_num):
+        loop = asyncio.get_event_loop()
+        tasks = []
+        for chunk in range(num_threads):
+            wav_idx = num_threads*iter_idx + chunk
+            if wav_idx >= len(wav_paths):
+                break
+            logger.info("%s/%s - %s", wav_idx+1, len(wav_paths), wav_paths[wav_idx])
+            task = loop.create_task(_async_all_in_one_extract(
+                wav_paths[wav_idx], label_path_mapping, feat_settings
+            ))
+            task.append(task)
+        
+        group = asyncio.gather(*task, return_exceptions=True)
+        print("Waiting...")
+        results = loop.run_until_complete(group)
+        for result in results:
+            patch_cqt, m_beat_arr, label_128, label_13, wav_path = result
+            basename = os.path.basename(wav_path)
+            filename, _ = os.path.splitext(basename)
+            out_hdf = jpath(out_path, filename + ".hdf")
+            with h5py.File(out_hdf, "w") as out_f:
+                out_f.create_dataset("feature", data=patch_cqt, compression="gzip", compression_opts=3)
+                out_f.create_dataset("label", data=label_13, compression="gzip", compression_opts=3)
+                out_f.create_dataset("label_128", data=label_128, compression="gzip", compression_opts=3)
+                out_f.create_dataset("mini_beat_arr", data=m_beat_arr, compression="gzip", compression_opts=3)
+
+
+async def _async_all_in_one_extract(wav_path, label_path_mapping, feat_settings):
+    loop = asyncio.get_event_loop()
+    patch_cqt, m_beat_arr, label_128, label_13 = await loop.run_in_executor(
+        None, _all_in_one_extract, wav_path, label_path_mapping, feat_settings
+    )
+    return patch_cqt, m_beat_arr, label_128, label_13, wav_path
+
+
 def _all_in_one_extract(wav_path, label_path_mapping, feat_settings):
     patch_cqt, m_beat_arr = extract_patch_cqt(
         wav_path, sampling_rate=feat_settings.sampling_rate, hop_size=feat_settings.hop_size
@@ -170,6 +289,25 @@ def _gen_wav_label_path_mapping(label_paths):
         wav_name = f_name.replace("align_mid", "ytd_audio")
         mapping[wav_name] = label_path
     return mapping
+
+
+def _loss_func(target, pred, soft_loss_range=20):
+    recon_error = tf.abs(target - pred)
+    recon_error_soft = tf.compat.v1.where(
+        recon_error <= soft_loss_range * tf.ones_like(recon_error),
+        tf.zeros_like(recon_error),
+        recon_error - soft_loss_range * tf.ones_like(recon_error)
+    )
+
+    shape = shape_list(recon_error_soft[:,:,:,:])
+    note_priority_arr = tf.constant(NOTE_PRIORITY_ARRAY, dtype=recon_error.dtype)
+    note_priority_ary_in_expanded = note_priority_arr + tf.zeros(shape, dtype=note_priority_arr.dtype)
+    recon_error_soft_priority = tf.multiply(recon_error_soft, note_priority_ary_in_expanded)
+    recon_error_soft_flat = tf.reshape(
+        recon_error_soft_priority, 
+        [-1, tf.keras.backend.prod(recon_error_soft_priority.get_shape()[1:])]
+    )
+    return tf.reduce_mean(input_tensor=recon_error_soft_flat)
 
 
 if __name__ == "__main__":
