a8eb777e5f81a4a2557244314cea39112889d221
BreezeWhite <freedombluewater@gmail.com>
2020-11-04 16:32:52 +0800

    Add vamp plugins and export env path to it

--- omnizart/__init__.py
+++ omnizart/__init__.py
@@ -1,8 +1,10 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 MODULE_PATH = os.path.abspath(__file__ + "/..")
 SETTING_DIR = os.path.join(MODULE_PATH, "defaults")
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['VAMP_PATH'] = os.path.join(MODULE_PATH, "resource", "vamp")
 
 __version__ = "0.1.0"
