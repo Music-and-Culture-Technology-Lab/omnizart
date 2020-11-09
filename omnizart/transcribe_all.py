28cc78a419ad45341bb64e077ef78d207b4d1c41
BreezeWhite <freedombluewater@gmail.com>
2020-11-09 13:45:13 +0800

    Fix lint error

--- omnizart/transcribe_all.py
+++ omnizart/transcribe_all.py
@@ -1,8 +1,8 @@
-import pretty_midi
+# import pretty_midi
 
-from omnizart.music import app as music_app
-from omnizart.drum import app as drum_app
-from omnizart.chord import app as chord_app
+# from omnizart.music import app as music_app
+# from omnizart.drum import app as drum_app
+# from omnizart.chord import app as chord_app
 
 
 def process(input_audio, model_path=None, output="./"):
