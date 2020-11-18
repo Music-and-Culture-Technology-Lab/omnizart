d26b379ea27576dea70f1de5a6f683dbbb21b27c
BreezeWhite <freedombluewater@gmail.com>
2020-11-18 15:32:55 +0800

    Update

--- omnizart/setting_loaders.py
+++ omnizart/setting_loaders.py
@@ -206,3 +206,53 @@ class ChordSettings(Settings):
             self.early_stop: int = None
             self.init_learning_rate: float = None
             self.learning_rate_decay: float = None
+
+
+@json_serializable(key_path="./General", value_path="./Value")
+class VocalSettings(Settings):
+    default_setting_file: str = "vocal.yaml"
+
+    def __init__(self, conf_path=None):
+        self.transcription_mode: str = None
+        self.checkpoint_path: dict = None
+        self.feature = self.VocalFeature()
+        self.dataset = self.VocalDataset()
+        self.model = self.VocalModel()
+        self.training = self.VocalTraining()
+
+        super().__init__(conf_path=conf_path)
+
+    @json_serializable(key_path="./Settings", value_path="./Value")
+    class VocalFeature():
+        def __init__(self):
+            self.hop_size: float = None
+            self.sampling_rate: int = None
+            self.window_size: int = None
+            self.frequency_resolution: float = None
+            self.frequency_center: float = None
+            self.time_center: float = None
+            self.gamma: list = None
+            self.bins_per_octave: int = None
+
+    @json_serializable(key_path="./Settings", value_path="./Value")
+    class VocalDataset():
+        def __init__(self):
+            self.save_path: str = None
+            self.feature_save_path: str = None
+
+    @json_serializable(key_path="./Settings", value_path="./Value")
+    class VocalModel():
+        def __init__(self):
+            self.save_prefix: str = None
+            self.save_path: str = None
+
+    @json_serializable(key_path="./Settings", value_path="./Value")
+    class VocalTraining():
+        def __init__(self):
+            self.epoch: int = None
+            self.steps: int = None
+            self.val_steps: int = None
+            self.batch_size: int = None
+            self.val_batch_size: int = None
+            self.early_stop: int = None
+            self.init_learning_rate: float = None
