ebea6c2b7ec4f6ea90dc64f5036b94ee141c4fba
Tsung-Ping <40154075+Tsung-Ping@users.noreply.github.com>
2021-12-29 10:49:09 +0800

    Add WarmupSchedule

--- omnizart/models/chord_model.py
+++ omnizart/models/chord_model.py
@@ -1074,6 +1074,22 @@ class ReduceSlope(tf.keras.callbacks.Callback):
         self.model.step_in_slope()
 
 
+class WarmupSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    """Custom keras callback for warm-up training."""
+    def __init__(self, d_model, warmup_steps=4000):
+        super(CustomSchedule, self).__init__()
+
+        self.d_model = d_model
+        self.d_model = tf.cast(self.d_model, tf.float32)
+        self.warmup_steps = warmup_steps
+
+    def __call__(self, step):
+        step += 1
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+
 if __name__ == "__main__":
     model = ChordModel()
     model.compile(optimizer="adam", loss=tf.keras.losses.BinaryCrossentropy())
