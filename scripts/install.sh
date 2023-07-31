fe1651cdfb19453996f49ae4d9003ebcc5d28e23
Chin-Yun Yu <ya70201@gmail.com>
2023-07-31 16:14:36 +0800

    remove commented commands

--- scripts/install.sh
+++ scripts/install.sh
@@ -60,10 +60,6 @@ install_with_pip() {
     # Install some tricky packages that cannot be resolved by setup.py
     # and requirements.txt.
     pip install Cython numpy==1.19.2
-    # pip install madmom
-
-    # pip install -r requirements.txt
-    # python3 setup.py install 
     pip install .
 }
 
