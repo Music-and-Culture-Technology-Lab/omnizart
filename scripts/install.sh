7130a141eb69edfe652ce4810ec236396239233a
Chin-Yun Yu <ya70201@gmail.com>
2023-07-31 15:44:19 +0800

    remove numpy and madmom

--- scripts/install.sh
+++ scripts/install.sh
@@ -59,8 +59,8 @@ install_with_poetry() {
 install_with_pip() {
     # Install some tricky packages that cannot be resolved by setup.py
     # and requirements.txt.
-    pip install Cython numpy
-    pip install madmom
+    pip install Cython #numpy
+    # pip install madmom
 
     # pip install -r requirements.txt
     # python3 setup.py install 
