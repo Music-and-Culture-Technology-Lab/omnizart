bcd8cb44d4da66ce87df10b6abee5c35a8cc2886
BreezeWhite <miyashita2010@tuta.io>
2026-05-31 15:08:03 +0800

    bump: version bump to v0.6.3

--- omnizart/__init__.py
+++ omnizart/__init__.py
@@ -20,4 +20,4 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['TF_USE_LEGACY_KERAS'] = '1'
 os.environ['VAMP_PATH'] = os.path.join(MODULE_PATH, "resource", "vamp")
 
-__version__ = "0.6.2"
+__version__ = "0.6.3"
